6836601d6043180c68a90c6b9e5d6927ad205027
PM99 <rasolarmand114@gmail.com>
2026-09-08 08:53:29 +0330

    Update build-android-aarch64.sh

--- scripts/build-android-aarch64.sh
+++ scripts/build-android-aarch64.sh
@@ -10,5 +10,6 @@ tar -xJf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz --strip-componen
 chmod +x waf
 chmod +x $HOME/llvm11/bin/llvm-strip
 export PATH=$HOME/llvm11/bin:$PATH
+export -DHAVE_ASTCENC=1
 CFLAGS="-O2" CXXFLAGS="-O2" LDFLAGS="-s -flto" ./waf configure -T release --build-games=csso --togles --android=aarch64,host,21 --prefix=./output --disable-warns &&
 ./waf install
